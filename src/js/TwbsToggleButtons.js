30b526d15ad7bc9b95bec62bae2a53af3fa9e69a
satpal.singh <satpal.singh@hearing.com.au>
2019-07-29 16:29:04 +1000

    Added fixes description

--- src/js/TwbsToggleButtons.js
+++ src/js/TwbsToggleButtons.js
@@ -1,4 +1,4 @@
-/** @preserve Twitter Bootstrap Toogle Buttons 0.0.2
+/** @preserve Twitter Bootstrap Toogle Buttons 0.0.3
  * Available under the MIT license.
  * See https://github.com/prokki/twbs-toggle-buttons for more information.
  */
